ac40df104a8b1f5a8b71e23746daa43f1cc6bc98
Alex <3152997-al3xbr0@users.noreply.gitlab.com>
2021-04-09 17:09:40 +0300

    Add some model classes

---
+++ src/main/java/ru/w/automation/domain/Column.java
@@ -0,0 +1,22 @@
+package ru.w.automation.domain;
+
+public class Column {
+
+    private final String name;
+    private final String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Column(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    //TODO
+}
---
+++ src/main/java/ru/w/automation/domain/ValidationStatus.java
@@ -0,0 +1,13 @@
+package ru.w.automation.domain;
+
+public class ValidationStatus {
+    private boolean schemeValid;
+    private boolean tableValid;
+    private boolean columnDuplicates;
+    private boolean columnsExist;
+    private boolean validationSucceed;
+
+    private String validationComment;
+
+    //TODO
+}
