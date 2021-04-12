f82dec705e84ce7729cba9c267ed3ff7bf19f77d
Alex <3152997-al3xbr0@users.noreply.gitlab.com>
2021-04-13 02:08:24 +0300

    Configure two data sources

--- src/main/java/ru/w/automation/config/DataSourceConfig.java
+++ src/main/java/ru/w/automation/config/DataSourceConfig.java
@@ -1,32 +1,27 @@
 package ru.w.automation.config;
 
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
 
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
+    @Bean("storageDataSource")
+    @Primary
+    @ConfigurationProperties("datasource.main")
+    public DataSource storageDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
 
-    @Bean
-    public DataSource dataSource() {
-        final HikariConfig config = new HikariConfig();
-        config.setDriverClassName(driverClassName);
-        config.setJdbcUrl(url);
-        config.setUsername(username);
-        config.setPassword(password);
-        return new HikariDataSource(config);
+    @Bean("camundaBpmDataSource")
+    @ConfigurationProperties("datasource.camunda")
+    public DataSource camundaBpmDataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
     }
 }
