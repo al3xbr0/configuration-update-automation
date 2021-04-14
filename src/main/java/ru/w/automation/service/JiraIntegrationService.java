51aa0a2461fee49229ab1fa0e93b7ad8a535f12e
Alex <3152997-al3xbr0@users.noreply.gitlab.com>
2021-04-14 04:22:00 +0300

    Provide storage for active and waiting issues

--- src/main/java/ru/w/automation/service/JiraIntegrationService.java
+++ src/main/java/ru/w/automation/service/JiraIntegrationService.java
@@ -3,12 +3,14 @@ package ru.w.automation.service;
 import com.atlassian.jira.rest.client.api.IssueRestClient;
 import com.atlassian.jira.rest.client.api.MetadataRestClient;
 import com.atlassian.jira.rest.client.api.domain.Comment;
-import com.atlassian.jira.rest.client.api.domain.Field;
 import com.atlassian.jira.rest.client.api.domain.Issue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class JiraIntegrationService {
@@ -23,6 +25,9 @@ public class JiraIntegrationService {
     @Autowired
     private MetadataRestClient metadataRestClient;
 
+    private final Set<String> issuesActive = new HashSet<>();
+    private final Set<String> issuesWaiting = new HashSet<>();
+
     public void addComment(String issueKey, String commentText) {
         Issue issue = getIssue(issueKey);
         LOGGER.info("Sending comment to issue {}", issueKey);
@@ -36,8 +41,19 @@ public class JiraIntegrationService {
         return issue;
     }
 
-    public Iterable<Field> getFields() {
-        return metadataRestClient.getFields().claim();
+    public boolean addActive(String issueKey) {
+        return issuesActive.add(issueKey);
     }
 
+    public boolean removeActive(String issueKey) {
+        return issuesActive.remove(issueKey);
+    }
+
+    public void addWaiting(String issueKey) {
+        issuesWaiting.add(issueKey);
+    }
+
+    public boolean removeWaiting(String issueKey) {
+        return issuesWaiting.remove(issueKey);
+    }
 }
