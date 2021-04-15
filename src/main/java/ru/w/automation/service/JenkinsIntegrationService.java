2345217932a72b117d80ebfae8922894406a61fa
Alex <3152997-al3xbr0@users.noreply.gitlab.com>
2021-04-16 00:24:38 +0300

    Add mock for Jenkins Release

---
+++ src/main/java/ru/w/automation/service/JenkinsIntegrationService.java
@@ -0,0 +1,12 @@
+package ru.w.automation.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@SuppressWarnings({"unused", "EmptyMethod"})
+public class JenkinsIntegrationService {
+
+    public void releaseTask(String jsonConfigName) {
+        //some code
+    }
+}
