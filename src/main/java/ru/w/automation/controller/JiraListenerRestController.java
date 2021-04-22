a258dc3f8b48cca63d65f514e679706e8b0695f2
Alex <3152997-al3xbr0@users.noreply.gitlab.com>
2021-04-22 20:21:16 +0300

    Improve controller, add logging

--- src/main/java/ru/w/automation/controller/JiraListenerRestController.java
+++ src/main/java/ru/w/automation/controller/JiraListenerRestController.java
@@ -1,35 +1,43 @@
 package ru.w.automation.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.w.automation.service.ProcessInstanceService;
 
-@SuppressWarnings("unused")
 @RestController
+@SuppressWarnings("unused")
 public class JiraListenerRestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JiraListenerRestController.class);
 
     @Autowired
     private ProcessInstanceService processInstanceService;
 
-    @PostMapping("api/jira/create/{issueKey:[a-zA-Z]+-\\d+}")
+    @PostMapping("api/jira/created/{issueKey:[a-zA-Z]+-\\d+}")
     public void created(@PathVariable("issueKey") String issueKey) {
+        LOGGER.info("Requested POST /api/jira/created/{}", issueKey);
         processInstanceService.startProcessInstance(issueKey);
     }
 
-    @PostMapping("api/jira/update/{issueKey:[a-zA-Z]+-\\d+}")
+    @PostMapping("api/jira/updated/{issueKey:[a-zA-Z]+-\\d+}")
     public void updated(@PathVariable("issueKey") String issueKey) {
+        LOGGER.info("Requested POST /api/jira/updated/{}", issueKey);
         processInstanceService.updateProcessInstance(issueKey);
     }
 
     @PostMapping("api/jira/canceled/{issueKey:[a-zA-Z]+-\\d+}")
     public void canceled(@PathVariable("issueKey") String issueKey) {
-        processInstanceService.continueProcessInstance(issueKey);
+        LOGGER.info("Requested POST /api/jira/canceled/{}", issueKey);
+        processInstanceService.cancelProcessInstance(issueKey);
     }
 
-    @PostMapping("api/jira/close/{issueKey:[a-zA-Z]+-\\d+}")
-    public void commentedWith(@PathVariable("issueKey") String issueKey) {
+    @PostMapping("api/jira/finished/{issueKey:[a-zA-Z]+-\\d+}")
+    public void finished(@PathVariable("issueKey") String issueKey) {
+        LOGGER.info("Requested POST /api/jira/finished/{}", issueKey);
         processInstanceService.finishProcessInstance(issueKey);
     }
 }
