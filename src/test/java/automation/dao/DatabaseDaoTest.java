50a144203115d0d3b03ae329a22a4ed6364445bd
Alex <3152997-al3xbr0@users.noreply.gitlab.com>
2021-05-20 02:31:44 +0300

    Update dao test

--- src/test/java/automation/dao/DatabaseDaoTest.java
+++ src/test/java/automation/dao/DatabaseDaoTest.java
@@ -24,16 +24,18 @@ class DatabaseDaoTest {
 
     @Test
     void tableExists() {
-        assertTrue(dao.tableExists("public", "test_table_always_exists"));
+        assertTrue(dao.tableExists("public", "test_users"));
         assertFalse(dao.tableExists("public", "no_such_test_table"));
     }
 
     @Test
     void tableDescription() {
         Set<Column> expected = new HashSet<>();
-        expected.add(new Column("test", "integer", null));
-        expected.add(new Column("test2", "character varying", 32));
-        Set<Column> actual = dao.tableDescription("public", "test_table_always_exists");
+        expected.add(new Column("id", "integer", null));
+        expected.add(new Column("first_name", "character varying", 32));
+        expected.add(new Column("last_name", "character varying", 32));
+        expected.add(new Column("address", "character varying", 64));
+        Set<Column> actual = dao.tableDescription("public", "test_users");
         assertEquals(expected, actual);
     }
 }
